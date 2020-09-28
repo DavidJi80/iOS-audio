8daf5a0b78c2bb601146234169cf3511d4f3c18e
David Ji <297933227@qq.com>
2020-09-28 09:43:36 +0800

    =

--- ios-audio/Model/AudioUnit-Play/AudioUnitPlayer.m
+++ ios-audio/Model/AudioUnit-Play/AudioUnitPlayer.m
@@ -9,6 +9,29 @@
 
 @implementation AudioUnitPlayer
 
+// 播放音频队列回调
+static OSStatus renderCallback(void *inRefCon,
+                               AudioUnitRenderActionFlags *ioActionFlags,
+                               const AudioTimeStamp *inTimeStamp,
+                               UInt32 inBusNumber,
+                               UInt32 inNumberFrames,
+                               AudioBufferList *ioData) {
+    NSLog(@"inBusNumber:%u,inNumberFrames:%u",(unsigned int)inBusNumber,(unsigned int)inNumberFrames);
+    AudioUnitPlayer *auPlayer = (__bridge AudioUnitPlayer *)(inRefCon);        // 1
+//    UInt32 numBytesReadFromFile;                              // 3
+//    UInt32 numPackets;           // 4
+//    OSStatus status=AudioFileReadPacketData(                            // 1
+//                                            auPlayer->mAudioFile,        // 2
+//                            false,                      // 3
+//                            &numBytesReadFromFile,      // 4
+//                                            auPlayer->mPacketDescs,      // 5
+//                                            auPlayer->mCurrentPacket,    // 6
+//                            &numPackets,                // 7
+//                            inBuffer->mAudioData        // 8
+//                            );
+    return noErr;
+}
+
 -(id)initWithAudio:(NSString *)path{
     //1.1
     CFURLRef audioFileURL =(__bridge CFURLRef)[NSURL fileURLWithPath:path];
@@ -68,6 +91,30 @@
     AUGraphNodeInfo (processingGraph, ioNode, NULL, &ioUnit);
 
     //4. 配置音频单元
+    status = AudioUnitSetProperty(ioUnit,
+                                  kAudioUnitProperty_StreamFormat,
+                                  kAudioUnitScope_Input,
+                                  0,
+                                  &mDataFormat,
+                                  sizeof(mDataFormat));
+    
+    AURenderCallbackStruct callbackStruct;
+    callbackStruct.inputProc        = &renderCallback;
+    callbackStruct.inputProcRefCon  = (__bridge void * _Nullable)(self);
+     
+    AUGraphSetNodeInputCallback (processingGraph,
+                                 ioNode,
+                                 0,                 // output element
+                                 &callbackStruct);
+    Boolean graphUpdated;
+    AUGraphUpdate (processingGraph, &graphUpdated);
+    
+    
+    //8.
+    status = AUGraphInitialize (processingGraph);
+    // Check for error. On successful initialization, start the graph...
+    AUGraphStart (processingGraph);
+
     
     return self;
 }
@@ -81,12 +128,12 @@
  
     NSLog (@"  Sample Rate:         %10.0f",  asbd.mSampleRate);
     NSLog (@"  Format ID:           %10s",    formatIDString);
-    NSLog (@"  Format Flags:        %10X",    asbd.mFormatFlags);
-    NSLog (@"  Bytes per Packet:    %10d",    asbd.mBytesPerPacket);
-    NSLog (@"  Frames per Packet:   %10d",    asbd.mFramesPerPacket);
-    NSLog (@"  Bytes per Frame:     %10d",    asbd.mBytesPerFrame);
-    NSLog (@"  Channels per Frame:  %10d",    asbd.mChannelsPerFrame);
-    NSLog (@"  Bits per Channel:    %10d",    asbd.mBitsPerChannel);
+    NSLog (@"  Format Flags:        %10X",    (unsigned int)asbd.mFormatFlags);
+    NSLog (@"  Bytes per Packet:    %10u",    (unsigned int)asbd.mBytesPerPacket);
+    NSLog (@"  Frames per Packet:   %10u",    (unsigned int)asbd.mFramesPerPacket);
+    NSLog (@"  Bytes per Frame:     %10u",    (unsigned int)asbd.mBytesPerFrame);
+    NSLog (@"  Channels per Frame:  %10u",    (unsigned int)asbd.mChannelsPerFrame);
+    NSLog (@"  Bits per Channel:    %10u",    (unsigned int)asbd.mBitsPerChannel);
 }
 
 @end
